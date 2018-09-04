22563193a1ca3a02325fd121d919588053d20576
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-04 11:06:52 +0200

    Andere Parameter

--- src/main/groovy/scheduleBranchPipelineForJobsOfView.groovy
+++ src/main/groovy/scheduleBranchPipelineForJobsOfView.groovy
@@ -4,27 +4,24 @@ import jenkins.model.*
 import groovy.xml.*
 import groovy.json.*
 
-
-
-
 def thr = Thread.currentThread()
 // get current build
 def build = thr?.executable
 def resolver = build.buildVariableResolver
 def viewName = resolver.resolve("VIEWNAME")
+def rootBranch = resolver.resolve("ROOTBRANCH")
+def targetBranch = resolver.resolver("TARGETBRANCH")
 def moduleList = []
 hudson.model.Hudson.instance.getView(viewName).items.each()  { job ->
-	println "Processing Job " + job.name 
 	def configXMLFile = job.getConfigFile().getFile().getAbsolutePath();
 	def configXml = new XmlSlurper().parse(configXMLFile)
-	println "Modulename : "
 	// TODO (che, 4.9.2018) : could be more then one
 	def remoteName =  configXml.depthFirst().find{ node -> node.name() == 'remoteName'}
 	moduleList << [moduleName:"${remoteName.toString()}"]
 	println remoteName
- 
+
 }
 println moduleList
-def jsonOutput = new JsonBuilder(moduleList)
+def jsonOutput = new JsonBuilder(["rootBranch":rootBranch, "targetBranch":targetBranch,modules:moduleList])
 println jsonOutput
 
