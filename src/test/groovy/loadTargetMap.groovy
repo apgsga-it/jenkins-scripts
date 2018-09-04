8bfde99f97148ea736c77b8c57c0d131272b37b5
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-04 09:38:20 +0200

    Eclipse config

--- src/test/groovy/loadTargetMap.groovy
+++
@@ -1,7 +0,0 @@
-import groovy.json.JsonSlurper
-
-def targetSystemFile = new File("src/test/resources/json", "TargetSystemMappings.json")
-def jsonSystemTargets = new JsonSlurper().parseText(targetSystemFile.text)
-def targetSystemMap = [:]
-jsonSystemTargets.targetSystems.each( { target -> targetSystemMap.put("$target.name", new Expando(envName:"$target.name",target:"${target.target}",targetTypeInd:"${target.targettype}"))})
-	println targetSystemMap 
