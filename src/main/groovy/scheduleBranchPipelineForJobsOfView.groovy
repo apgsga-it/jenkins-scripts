cdb2ba32dcf26497c054b57e0a0e05e8173a0d80
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-04 10:39:35 +0200

    Json Loop

--- src/main/groovy/scheduleBranchPipelineForJobsOfView.groovy
+++ src/main/groovy/scheduleBranchPipelineForJobsOfView.groovy
@@ -20,7 +20,7 @@ hudson.model.Hudson.instance.getView(viewName).items.each()  { job ->
 	println "Modulename : "
 	// TODO (che, 4.9.2018) : could be more then one
 	def remoteName =  configXml.depthFirst().find{ node -> node.name() == 'remoteName'}
-	moduleList << [name:remoteName]
+	moduleList << [moduleName:remoteName]
 	println remoteName
  
 }
