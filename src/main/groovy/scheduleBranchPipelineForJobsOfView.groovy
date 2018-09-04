9184fa0d890f06548b1bb9ca7018c1880697ba87
Christoph Henrici <chhenrici@gmail.com>
2018-09-04 07:32:59 +0200

    Create new Branch

---
+++ src/main/groovy/scheduleBranchPipelineForJobsOfView.groovy
@@ -0,0 +1,22 @@
+import hudson.*
+import hudson.model.*
+import groovy.xml.*
+import javax.xml.transform.Source
+import javax.xml.transform.stream.StreamSource
+
+
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
+def resolver = build.buildVariableResolver
+def viewName = resolver.resolve("VIEWNAME")
+
+def patchView = Hudson.instance.getView(viewName)
+patchView.each { job ->
+	println "Processing Job " + job.fullName + " start"
+	def configXMLFile = job.getConfigFile().getFile().getAbsolutePath();
+	def configXml = new XmlSlurper().parse(configXMLFile)
+	println "Configuration : "
+	println XmlUtil.serialize(configXml).toString()
+  
+}
