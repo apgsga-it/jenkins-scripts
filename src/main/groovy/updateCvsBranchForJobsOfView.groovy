a6f1fc8256289a3d230110fdeb036104857c94f3
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-06 11:55:53 +0200

    Branchname

--- src/main/groovy/updateCvsBranchForJobsOfView.groovy
+++ src/main/groovy/updateCvsBranchForJobsOfView.groovy
@@ -12,16 +12,15 @@ def viewName = resolver.resolve("VIEWNAME")
 def targetBranch = resolver.resolve("TARGETBRANCH")
 
 
-def moduleList = []
+
 hudson.model.Hudson.instance.getView(viewName).items.each()  { job ->
 	def configXMLFile = job.getConfigFile().getFile().getAbsolutePath();
 	def configXml = new XmlSlurper().parse(configXMLFile)
 	println XmlUtil.serialize(configXml).toString()
 	println " "
 	// TODO (che, 4.9.2018) : could be more then one
-	def remoteName =  configXml.depthFirst().find{ node -> node.name() == 'remoteName'}
-	moduleList << [moduleName:"${remoteName.toString()}"]
-	println remoteName
+	def branchName =  configXml.depthFirst().find{ node -> node.name() == 'locationName'}
+	println branchName
 
 }
 
