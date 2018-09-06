89ed8c2852dc2a8e963529c1e8332a75424271ef
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-06 11:28:56 +0200

    Got rid of +

--- src/main/groovy/createNewBranchForModulesPipeline.groovy
+++ src/main/groovy/createNewBranchForModulesPipeline.groovy
@@ -13,7 +13,7 @@ def request = new JsonSlurperClassic().parseText(params.PARAMETER)
 
 stage ("Cvs Branching Modules") {
 	def parallelBranching = request.modules.collectEntries {
-		[ "Module ${it}" : branchModule(it, request.rootBranch, request.targetBranch)]
+		[ "Module ${it.moduleName}" : branchModule(it, request.rootBranch, request.targetBranch)]
 	}
 	parallel parallelBranching
 }
@@ -25,7 +25,7 @@ def branchModule(module,rootBranch,targetBranch) {
 			sh "#!/bin/bash \n" + 
 			"set -x\n" +
 			"export CVSROOT=/var/local/cvs/root\n" +
-			"cvs rtag -b -r ${rootBranch} ${targetBranch} + ${module.moduleName}"
+			"cvs rtag -b -r ${rootBranch} ${targetBranch} ${module.moduleName}"
 		}
 	}
 }
