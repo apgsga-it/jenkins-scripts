069e53187321e042c0010144665bb74a2d27d86a
Julien Helbling <Julien Helbling@LAZHG552433012.affichage-p.ch>
2018-10-08 15:58:18 +0200

    Better use of dryRun

--- src/main/groovy/reinitilizeJenkins.groovy
+++ src/main/groovy/reinitilizeJenkins.groovy
@@ -26,20 +26,27 @@ stage("Delete all Patch Job with corresponding Json file") {
 		["ProductivePatches", "Patches"].each { viewName ->
 		
 			Jenkins.instance.getView(viewName).items.each { item ->
-				if (!dry) {
-					def jobName = item.name
+				def jobName = item.name
+				if(!dryRun) {
 					item.delete()
 					println "Deleted ${item.name}"
-					if (!jobName.contains("Download")) {
-						def patchNumber = jobName.substring(5,jobName.length())
-						def cmd = "/opt/apg-patch-cli/bin/apscli.sh -r ${patchNumber}"
-						if(new File("/var/opt/apg-patch-service-server/db/Patch${patchNumber}.json").exists()) {
-							println "Following ccommand will be executed: ${cmd}"
+				}
+				else{
+					println "dryRun only ... ${item.name} would have been deleted."
+				}
+				
+				if (!jobName.contains("Download")) {
+					def patchNumber = jobName.substring(5,jobName.length())
+					def cmd = "/opt/apg-patch-cli/bin/apscli.sh -r ${patchNumber}"
+					if(new File("/var/opt/apg-patch-service-server/db/Patch${patchNumber}.json").exists()) {
+						println "Following ccommand will be executed: ${cmd}"
+						if(!dry) {
 							sh(cmd)
 						}
+						else {
+							println "dryRun only ... ${cmd} has not been executed."
+						}
 					}
-				} else {
-					println "Didn't do anything for ${item.name}, running dry ..."
 				}
 			}
 		}
@@ -48,73 +55,83 @@ stage("Delete all Patch Job with corresponding Json file") {
 	
 stage("Delete Revisions.json and patchToBeReinstalled.json files") {
 	node {
-		if(!dry) {
-			def msg
-			def revisionFileName = "/var/opt/apg-patch-cli/Revisions.json"
-			def revisionFile = new File(revisionFileName)
-			def patchToBeInstalledFileName = "/var/opt/apg-patch-cli/patchToBeReinstalled.json"
-			def patchToBeInstalledFile = new File(patchToBeInstalledFileName)
-			
-			if(revisionFile.exists()) {
+		def msg
+		def revisionFileName = "/var/opt/apg-patch-cli/Revisions.json"
+		def revisionFile = new File(revisionFileName)
+		def patchToBeInstalledFileName = "/var/opt/apg-patch-cli/patchToBeReinstalled.json"
+		def patchToBeInstalledFile = new File(patchToBeInstalledFileName)
+		
+		if(revisionFile.exists()) {
+			if(!dryRun) {
 				msg = (revisionFile.delete() ? "${revisionFileName} has been deleted!" : "Error while deleting ${revisionFileName}")
 				println msg
 			}
-			
-			if(patchToBeInstalledFile.exists()) {
+			else {
+				println "dryRun only ... ${revisionFileName} would have been deleted."
+			}
+		}
+		
+		if(patchToBeInstalledFile.exists()) {
+			if(!dryRun) {
 				msg = (patchToBeInstalledFile.delete() ? "${patchToBeInstalledFileName} has been deleted!" : "Error while deleting ${patchToBeInstalledFileName}")
 				println msg
 			}
-		} else {
-			println "Revisions.json and patchToBeReinstalled.json files haven't been deleted, running dry ..."
+			else {
+				println "dryRun only ... ${patchToBeInstalledFileName} would have been deleted."
+			}
 		}
 	}
 }
 
 stage("Cleaning up Jenkins Maven Local Repository") {
 	node {
-		if(!dry) {
-			def affichageFolder = "/var/jenkins/maven/repository/com/affichage"
-			def apgsgaFolder = "/var/jenkins/maven/repository/com/apgsga"
-			
+		def affichageFolder = "/var/jenkins/maven/repository/com/affichage"
+		def apgsgaFolder = "/var/jenkins/maven/repository/com/apgsga"
+
+		if(!dryRun) {
 			sh("rm -rf ${affichageFolder}")
 			sh("rm -rf ${apgsgaFolder}")
-			
 			println "${affichageFolder} and ${apgsgaFolder} have been deleted!"
 		}
 		else {
-			println "Jenkins Maven Local Repository has not been cleaned up, running dry ..."
+			println "dryRun only ... ${affichageFolder} and ${apgsgaFolder} would have been deleted."
 		}
 	}
 }
 
 stage("Cleaning up Artifactory releases") {
 	node {
-		if(!dry) {
-			println "All Artifacty for ${releaseArtifactToDelete} Release will be deleted..."
-			
-			def fileName = "artifactsToDelete.json"
-			def f = new File(fileName)
-			if(f.exists()) {
+		println "All Artifacty for ${releaseArtifactToDelete} Release will be deleted..."
+		
+		def fileName = "artifactsToDelete.json"
+		def f = new File(fileName)
+		if(f.exists()) {
+			if(!dryRun) {
 				f.delete()
 			}
-			def cmd = "curl -o ${fileName} -udev:dev1234 \"https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/api/search/artifact?name=${releaseArtifactToDelete}*&repos=releases\""
-			sh(cmd)
-			f = new File("/var/jenkins/workspace/Reinitialize Jenkins/artifactsToDelete.json")
-			JsonSlurper slurper = new JsonSlurper()
-			def artifacts = slurper.parse(f)
+			else{
+				println "dryRun only ... ${fileName} would have been deleted."
+			}
+		}
+		def cmd = "curl -o ${fileName} -udev:dev1234 \"https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/api/search/artifact?name=${releaseArtifactToDelete}*&repos=releases\""
+		sh(cmd)
+		f = new File("/var/jenkins/workspace/Reinitialize Jenkins/artifactsToDelete.json")
+		JsonSlurper slurper = new JsonSlurper()
+		def artifacts = slurper.parse(f)
+		
+		artifacts.results.each{
 			
-			artifacts.results.each{
-				
-				def urlforDelete = "https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/releases/"
-				def firstIndex = "https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/api/storage/releases/"
-				def lastPartUrl = it.uri.substring(firstIndex.length(), it.uri.length())
-				
+			def urlforDelete = "https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/releases/"
+			def firstIndex = "https://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga/api/storage/releases/"
+			def lastPartUrl = it.uri.substring(firstIndex.length(), it.uri.length())
+			
+			if(!dryRun) {
 				println "Will be deleted: ${urlforDelete}${lastPartUrl}" 
 				sh("curl -udev:dev1234 -XDELETE ${urlforDelete}${lastPartUrl}")
 			}
-		}
-		else {
-			println "No Release have been deleted from Artifactory, running dry ..."
+			else {
+				println "dryRun only ... ${urlforDelete}${lastPartUrl} would have been deleted."
+			}
 		}
 	}
 }
