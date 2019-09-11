bb50194f39eb2f40e6d941c1b9d8be4dccc04ccf
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-11 08:44:46 +0200

    .

--- src/main/groovy/artifactoryCleaningAsPipeline.groovy
+++ src/main/groovy/artifactoryCleaningAsPipeline.groovy
@@ -1,6 +1,8 @@
 
 ["repo1","repo2"].each {repo -> 
 
+	def artifactory = ArtifactoryClientBuilder.create().setUrl(env.mavenRepoBaseUrl).setUsername(mavenRepoUser).setPassword(mavenRepoPwd).build();
+		
 	stage(repo) {
 		println "this was a test"
 	}
