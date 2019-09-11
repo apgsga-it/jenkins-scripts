4f5728ae462da8ac1e51f49ddbd7b15d5f34ecd3
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-11 08:06:35 +0200

    .

--- src/main/groovy/artifactoryCleaning.groovy
+++ src/main/groovy/artifactoryCleaning.groovy
@@ -2,6 +2,6 @@ import org.jfrog.*
 
 println "Starting to clean Artifactory Repositories ...."
 
-
+artifactory = ArtifactoryClientBuilder.create().setUrl("url").setUsername("oneuser").setPassword("onepwd").build();
 
 println "Cleaning Artifactory Repositories - DONE"
