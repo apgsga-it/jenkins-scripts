00fd4693ef835677fb3ff9baf6ac311cf6176326
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-11 15:02:57 +0200

    .

--- src/main/groovy/artifactoryCleaningAsPipeline.groovy
+++ src/main/groovy/artifactoryCleaningAsPipeline.groovy
@@ -8,10 +8,7 @@ import groovy.json.JsonSlurperClassic
 		
 		node {
 		
-			println "this was a test"
-
-			
-			def query = 'items.find({"type":"file","name":{"$match":"it21gui-dist-zip-9.1.0.ADMIN-UIMIG-106.zip"}})'
+			def query = 'items.find({"repo":"releases-test","type":"file","name":{"$match":"it21gui-dist-zip-9.1.0.ADMIN-UIMIG-9*.zip"}})'
 			def artifactoryUrl = "http://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga"
 			def searchRequestUrl = "${artifactoryUrl}/api/search/aql"
 			
@@ -53,9 +50,9 @@ import groovy.json.JsonSlurperClassic
 				}
 				
 				println "Following Artifact will be deleted (resultPath): ${resultPath}"
-				curlCmd = "curl -L -u ${repoUser}:${repoPwd} -X DELETE ${artifactoryUrl}/${resultPath}"
-				res = sh script:curlCmd, returnStdout:true
-				println "res from delete: ${res}"
+//				curlCmd = "curl -L -u ${repoUser}:${repoPwd} -X DELETE ${artifactoryUrl}/${resultPath}"
+//				res = sh script:curlCmd, returnStdout:true
+//				println "res from delete: ${res}"
 	
 			}			
 		}
