55629663b35ce5505af453ebdf39c881257bda45
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-11 10:17:25 +0200

    .

--- src/main/groovy/artifactoryCleaningAsPipeline.groovy
+++ src/main/groovy/artifactoryCleaningAsPipeline.groovy
@@ -1,15 +1,12 @@
 #!groovy
 
-@Grab('org.jfrog.artifactory.client:artifactory-java-client-services:2.6.0')
-
-import org.jfrog.artifactory.client.ArtifactoryClientBuilder
-
 ["repo1","repo2"].each {repo -> 
 
 	//def artifactory = ArtifactoryClientBuilder.create().setUrl(mavenRepoBaseUrl).setUsername(mavenRepoUser).setPassword(mavenRepoPwd).build();
 		
 	stage(repo) {
 		println "this was a test"
+		println "mavenRepoBaseUrl: ${mavenRepoBaseUrl}"
 	}
 	
 }
