dd55fe032a365288efb2418dc92110a770d5b1cd
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-11 11:13:26 +0200

    node added

--- src/main/groovy/artifactoryCleaningAsPipeline.groovy
+++ src/main/groovy/artifactoryCleaningAsPipeline.groovy
@@ -5,29 +5,33 @@
 	//def artifactory = ArtifactoryClientBuilder.create().setUrl(mavenRepoBaseUrl).setUsername(mavenRepoUser).setPassword(mavenRepoPwd).build();
 		
 	stage(repo) {
-		println "this was a test"
 		
+		node {
 		
-		
-		println "${env.ARTIFACTORY_SERVER_ID}"
-		
-//		def server = Artifactory.server env.ARTIFACTORY_SERVER_ID
-		def u
-		def p
-		
-			withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artifactoryDev',
-					usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-		
-				u = "${USERNAME}"
-				p = "${PASSWORD}"
-			}
-		
-		
-		println "${u} / ${p}"
-		
-		def res = sh script:"curl www.google.com", returnStatus:true
-		
-		println "res: ${res}"
+			println "this was a test"
+			
+			
+			
+			println "${env.ARTIFACTORY_SERVER_ID}"
+			
+	//		def server = Artifactory.server env.ARTIFACTORY_SERVER_ID
+			def u
+			def p
+			
+				withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artifactoryDev',
+						usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+			
+					u = "${USERNAME}"
+					p = "${PASSWORD}"
+				}
+			
+			
+			println "${u} / ${p}"
+			
+			def res = sh script:"curl www.google.com", returnStatus:true
+			
+			println "res: ${res}"
+		}
 		
 	}
 	
