bd63dae241dbad430eb30b474517692e2ecc5712
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-13 08:49:30 +0200

    .

--- src/main/groovy/artifactoryCleaningAsPipeline.groovy
+++ src/main/groovy/artifactoryCleaningAsPipeline.groovy
@@ -19,7 +19,14 @@ repositories.repositories.each {repo ->
 		
 		node {
 
-			def query = "items.find({\"repo\":\"${repo.name}\", \"created\":{\"\$lt\":\"${repo.maxFileDate}\"}, \"type\":\"file\", \"\$and\": [{\"name\":{\"\$match\":\"${repo.fileNamePattern}\"}},{\"name\":{\"\$nmatch\":\"*9.1.0.ADMIN-UIMIG-198*\"}}]})"
+			def excludedReleases = getExcludedReleases()
+
+			println "excludedReleases: ${excludedReleases}"
+						
+			def query = "items.find({\"repo\":\"${repo.name}\", \"created\":{\"\$lt\":\"${repo.maxFileDate}\"}, \"type\":\"file\", \"\$and\": [{\"name\":{\"\$match\":\"${repo.fileNamePattern}\"}}${getExcludeNameFilter()}]})"
+			
+			println "query: ${query}"
+			
 			def artifactoryUrl = "http://artifactory4t4apgsga.jfrog.io/${env.ARTIFACTORY_SERVER_ID}"
 			def searchRequestUrl = "${artifactoryUrl}/api/search/aql"
 			
@@ -55,3 +62,16 @@ repositories.repositories.each {repo ->
 	}
 }
 
+private def getExcludedReleases() {
+	def prodReleases = ["9.1.0.ADMIN-UIMIG-46","9.1.0.ADMIN-UIMIG-198","9.1.0.ADMIN-UIMIG-214","9.1.0.ADMIN-UIMIG-234","9.1.0.ADMIN-UIMIG-237","9.1.0.ADMIN-UIMIG-240","9.1.0.ADMIN-UIMIG-249","9.1.0.ADMIN-UIMIG-252","9.1.0.ADMIN-UIMIG-255","9.1.0.ADMIN-UIMIG-258","9.1.0.ADMIN-UIMIG-261"]
+	def releasesToBeExcluded
+	releasesToBeExcluded = prodReleases.stream().map{r -> getSingleAQLExcludeReleaseStatement(r)}.collect(Collectors.toList()).join("")
+	return releasesToBeExcluded
+}
+
+private def getSingleAQLExcludeReleaseStatement(def release) {
+	def firstPart = ",{\"name\":{\"\$nmatch\":\"*"
+	def extractedRelease = release.substring(release.lastIndexOf("-"),release.length()) + "."
+	def lastPart = "*\"}}"
+	return "${firstPart}${extractedRelease}${lastPart}"
+}
