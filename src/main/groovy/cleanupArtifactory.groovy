e9675460abe37160c72afe3ed0921a8ad25d6bfd
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-18 14:52:18 +0200

    .

--- src/main/groovy/cleanupArtifactory.groovy
+++ src/main/groovy/cleanupArtifactory.groovy
@@ -1,2 +1,20 @@
 
 println "salut"
+
+
+def getUserPassword = { username ->
+	def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+			com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
+			jenkins.model.Jenkins.instance
+			)
+
+	def c = creds.findResult { it.username == username ? it : null }
+
+	if ( c ) {
+		return c.password
+	} else {
+		println "could not find credential for ${username}"
+	}
+}
+
+println getUserPassword("artifactoryDev")
