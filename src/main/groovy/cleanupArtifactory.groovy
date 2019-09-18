777dd76557f4afedec37fb86ada454a1fedcb4d0
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-18 15:52:07 +0200

    .

--- src/main/groovy/cleanupArtifactory.groovy
+++ src/main/groovy/cleanupArtifactory.groovy
@@ -1,17 +1,30 @@
 
 
 def env = System.getenv()
-println env["artifactoryUser"]
-println env["artifactoryPassword"]
+def username = env["artifactoryUser"]
+def userpwd = env["artifactoryPassword"]
 
 println "trying to run a sh command ..."
 
-def proc = "curl www.google.ch".execute()
+
+def query = "items.find({\"repo\":\"releases-test\", \"created\":{\"\$lt\":\"2099-01-01\"}, \"type\":\"file\", \"name\":{\"\$match\":\"*.zip\"}"
+
+def artifactoryUrl = "http://artifactory4t4apgsga.jfrog.io/artifactory4t4apgsga"
+def searchRequestUrl = "${artifactoryUrl}/api/search/aql"
+
+def curlCmd
+def res
+
+curlCmd = "curl -L -u ${username}:${userpwd} -X POST -H \"Content-Type: text/plain\" -d '${query}' ${searchRequestUrl}"
+
+def proc = curlCmd.execute()
 def sout = new StringBuilder()
 def serr = new StringBuilder()
 proc.consumeProcessOutput(sout,serr)
 proc.waitForOrKill(5000)
-println "out: ${sout} ..... err: ${serr}"
+println "out: ${sout}"
+println "=========================================================================="
+println "err: ${serr}"
 
 
 println "Done"
