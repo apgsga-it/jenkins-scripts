d3bdccdc2573265146470ae7d4312c38c581c602
jhe <jhe@LAZHG552433282.affichage-p.ch>
2019-09-18 15:42:32 +0200

    .

--- src/main/groovy/cleanupArtifactory.groovy
+++ src/main/groovy/cleanupArtifactory.groovy
@@ -1,8 +1,17 @@
 
 
 def env = System.getenv()
-println "Start"
 println env["artifactoryUser"]
 println env["artifactoryPassword"]
-println env["adaskdlj"]
+
+println "trying to run a sh command ..."
+
+def proc = "ls".execute()
+def sout = new StringBuilder()
+def serr = new StringBuilder()
+proc.consumeProcessOutput(sout,serr)
+proc.waitForOrKill(5000)
+println "out: ${sout} ..... err: ${serr}"
+
+
 println "Done"
